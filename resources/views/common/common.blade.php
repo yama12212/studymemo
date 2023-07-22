759ab2f9569f45e6a49e6f99e13b6e13ece33c1f
yama12212 <t.yamanaka.sd@gmail.com>
2023-07-22 18:24:29 +0900

    fix: 必要なmetaタグを追加

--- resources/views/common/common.blade.php
+++ resources/views/common/common.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="勉強用のメモアプリです。テスト機能付きなのでアウトプットもできて勉強効率UP!!" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>@yield('title')</title>
+
 </head>
 <body>
 
