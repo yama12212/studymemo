7ebae0e026ddfe3ea0e98c8b465ddd4642a301b9
yama12212 <t.yamanaka.sd@gmail.com>
2023-07-22 18:42:22 +0900

    feat: cssファイル参照先を追加。font awesomeをCDNにて導入

--- resources/views/common/common.blade.php
+++ resources/views/common/common.blade.php
@@ -6,7 +6,8 @@
   <meta name="description" content="勉強用のメモアプリです。テスト機能付きなのでアウトプットもできて勉強効率UP!!" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
-
+  <link rel="stylesheet" href="{{ asset('/css/common.css') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
 
