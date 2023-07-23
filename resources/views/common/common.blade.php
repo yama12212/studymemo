290ba906f8897143411086407a8302d32d7d1ac4
yama12212 <t.yamanaka.sd@gmail.com>
2023-07-23 13:31:29 +0900

    fix: htmlレイアウト共通部分をcommonに格納

--- resources/views/common/common.blade.php
+++ resources/views/common/common.blade.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="勉強用のメモアプリです。テスト機能付きなのでアウトプットもできて勉強効率UP!!" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title')</title>
-  <link rel="stylesheet" href="{{ asset('/css/common.css') }}">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="勉強用のメモアプリです。テスト機能付きなのでアウトプットもできて勉強効率UP!!" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/css/common.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  </head>
 
-</body>
+  <body>
+    <header>
+
+    </header>
+
+    <main>
+      <div class="container">
+        @yield('content')
+      </div>
+    </main>
+
+    <footer>
+
+    </footer>
+  </body>
 </html>
