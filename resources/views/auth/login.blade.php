33c86180d3a623e7041efddac0c7b62caff50822
yama12212 <t.yamanaka.sd@gmail.com>
2023-07-23 22:39:46 +0900

    fix: 新規登録・ログイン画面の共通レイアウト実装

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
+@extends('common.common')
+
+@section('title', 'ログイン')
+@section('content')
+<div class="form">
   <p>ログイン</p>
   <form action="{{ route('user.login') }}", method="post">
     {{ csrf_field() }}
@@ -15,5 +11,5 @@
     パスワード:<input type="password" name="password">
     <button type="submit" name="action" value="send">送信</button>
   </form>
-</body>
-</html>
+</div>
+@endsection
