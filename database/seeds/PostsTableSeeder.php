71aa7bb90c4414e9edb791a87c5dd73be3eda5bc
yama12212 <t.yamanaka.sd@gmail.com>
2024-03-23 23:17:32 +0900

    fix:　テストseedを追加。論理削除に変更

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,6 +11,15 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('posts')->insert([
+            [
+                'title' => 'タイトルテストテストテスト',
+                'post' => 'メモ内容テストテストテスト'
+            ],
+            [
+                'title' => 'タイトルテストテストテスト2',
+                'post' => 'メモ内容テストテストテスト2'
+            ],
+        ]);
     }
 }
