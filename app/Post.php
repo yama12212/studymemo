b570a37cfaa00c34f6d1919dfa9c73951c7ba800
yama12212 <t.yamanaka.sd@gmail.com>
2024-04-04 23:10:23 +0900

    fix: postモデルのfillable、table変数を追加

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'post',
+        'user_id',
+        'note_id'
+    ];
+
+    protected $table = 'posts';
 }
