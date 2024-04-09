4ac5deddb45cd8bf5a4f9b8e9e74c60d64c896c8
yama12212 <t.yamanaka.sd@gmail.com>
2024-04-09 23:04:33 +0900

    fix: リスト一覧ページのクラスを統一化

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,31 @@
 @section('content')
 
   @guest
-  <div class="noteListGuestView">
-    <p class="noteListGuestText">ログインすると登録したノートが表示されます</p>
+  <div class="listGuestView">
+    <p class="listGuestText">ログインすると登録したノートが表示されます</p>
   </div>
   @endguest
 
   @auth
-  <div class="noteListViewArea">
-    <h2 class="noteListViewAreaLabel">ノート一覧</h2>
-    <div class="noteListViewAreaContainer content-width center">
-      <ul class="noteListViewAreaContents">
+  <div class="listViewArea">
+    <h2 class="listViewAreaLabel">ノート一覧</h2>
+    <div class="listViewAreaContainer content-width center">
+      <ul class="listViewAreaContents">
         {{-- 未ログイン→ログインするとメモが表示されます 未登録→登録しているメモはありませんを表示 --}}
-        <li class="noteListViewAreaContentLabel flex">
-          <p class="noteListViewAreaContentTitleLabel">ノートタイトル</p>
-          <p class="noteListViewAreaContentUpdateTimeLabel">更新日時</p>
-          <p class="noteListViewAreaContentIconLabel">編集/削除</p>
+        <li class="listViewAreaContentLabel flex">
+          <p class="listViewAreaContentTitleLabel">ノートタイトル</p>
+          <p class="listViewAreaContentUpdateTimeLabel">更新日時</p>
+          <p class="listViewAreaContentIconLabel">編集/削除</p>
         </li>
 
         @foreach($notes as $note)
         @if($note == null)
           <p>登録しているノートはありません</p>
         @else
-        <li class="noteListViewAreaContent flex">
-          <a href="/post/index/{{ $note->id }}" class="noteListViewAreaContentTitle">{{ $note->title }}</a>
-          <p class="noteListViewAreaContentUpdateTime">{{ date('Y/m/d', strtotime($note->updated_at)) }}</p>
-          <div class="noteListViewAreaContentIconLink">
+        <li class="listViewAreaContent flex">
+          <a href="/post/index/{{ $note->id }}" class="listViewAreaContentTitle">{{ $note->title }}</a>
+          <p class="listViewAreaContentUpdateTime">{{ date('Y/m/d', strtotime($note->updated_at)) }}</p>
+          <div class="listViewAreaContentIconLink">
             <a href="/note/show/{{ $note->id }}" class="textDecorationDisable">
               <i class="fa-sharp fa-regular fa-pen-to-square"></i>
             </a>
@@ -43,7 +43,7 @@
   </div>
 
   <a href="/note/new">
-    <div class="noteCreateButton">
+    <div class="createButton">
       ノートを新規作成<i class="fa-sharp fa-regular fa-pen-to-square"></i>
     </div>
   </a>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,22 +3,22 @@
 @section('content')
   {{-- メモ閲覧と編集ページを作る閲覧ページで赤シートを追加 --}}
   <h2>ノートの名前 メモ一覧</h2>
-  <div class="postListViewAreaContainer content-width center">
-    <ul class="postListViewAreaContents">
-      <li class="postListViewAreaContentLabel flex">
-        <p class="postListViewAreaContentTitleLabel">メモタイトル</p>
-        <p class="postListViewAreaContentUpdateTimeLabel">更新日時</p>
-        <p class="postListViewAreaContentIconLabel">閲覧/編集/削除</p>
+  <div class="listViewAreaContainer content-width center">
+    <ul class="listViewAreaContents">
+      <li class="listViewAreaContentLabel flex">
+        <p class="listViewAreaContentTitleLabel">メモタイトル</p>
+        <p class="listViewAreaContentUpdateTimeLabel">更新日時</p>
+        <p class="listViewAreaContentIconLabel">閲覧/編集/削除</p>
       </li>
 
       @foreach($posts as $post)
       @if($post == null)
         <p>登録しているメモはありません</p>
       @else
-      <li class="postListViewAreaContent flex">
-        <a href="#" class="postListViewAreaContentTitle">{{ $post->title }}</a>
-        <p class="postListViewAreaContentUpdateTime">{{ date('Y/m/d', strtotime($post->updated_at)) }}</p>
-        <div class="postListViewAreaContentIconLink">
+      <li class="listViewAreaContent flex">
+        <a href="#" class="listViewAreaContentTitle">{{ $post->title }}</a>
+        <p class="listViewAreaContentUpdateTime">{{ date('Y/m/d', strtotime($post->updated_at)) }}</p>
+        <div class="listViewAreaContentIconLink">
           <a href="#" class="textDecorationDisable">
             <i class="fa-sharp fa-regular fa-pen-to-square"></i>
           </a>
@@ -33,7 +33,7 @@
   </div>
 
   <a href="/post/new">
-    <div class="postCreateButton">
+    <div class="createButton">
       メモを新規作成<i class="fa-sharp fa-regular fa-pen-to-square"></i>
     </div>
   </a>
