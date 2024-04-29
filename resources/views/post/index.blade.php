55e1a5a18671d382275c58cafbcbabd7760628ad
yama12212 <t.yamanaka.sd@gmail.com>
2024-04-29 15:13:03 +0900

    fix: メモ一覧の遷移先を作成

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -16,7 +16,7 @@
         <p>登録しているメモはありません</p>
       @else
       <li class="listViewAreaContent flex listHover">
-        <a href="#" class="listViewAreaContentTitle">{{ $post->title }}</a>
+        <a href="/post/show/{{ $post->id }}" class="listViewAreaContentTitle">{{ $post->title }}</a>
         <p class="listViewAreaContentUpdateTime">{{ date('Y/m/d', strtotime($post->updated_at)) }}</p>
         <div class="listViewAreaContentIconLink">
           <a href="/post/view/{{ $post->id }}" class="textDecorationDisable">
